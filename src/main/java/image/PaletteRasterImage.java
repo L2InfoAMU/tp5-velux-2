3bfc71babc3e62e1532928084af677117556b4bb
Burnasse <tommy.lecourt@etu.univ-amu.fr>
2018-11-29 17:30:24 +0100

    Ajout des constructeurs PaletteRasterImage et de la méthod createRepresentation

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -3,24 +3,40 @@ package image;
 import javafx.scene.paint.Color;
 import util.Matrices;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PaletteRasterImage implements Image {
     int width;
     int height;
     List<Color> palette;
-    byte[][] indexesOfColors;
+    int[][] indexesOfColors;
 
     public PaletteRasterImage(Color color, int width, int height){
-
+        this.width = width;
+        this.height = height;
+        createRepresentation();
+        palette.add(color);
+        for(int i=0;i<width;i++){
+            for(int j = 0; j<height;j++)indexesOfColors[i][j]=1;
+        }
     }
 
     public PaletteRasterImage(Color[][] pixels){
-
+        height = Matrices.getColumnCount(pixels);
+        width = Matrices.getRowCount(pixels);
+        createRepresentation();
+        for(int i=0;i<width;i++){
+            for(int j = 0; j<height;j++){
+                if(!palette.contains(pixels[i][j])) palette.add(pixels[i][j]);
+                indexesOfColors[i][j] = palette.indexOf(pixels[i][j]);
+            }
+        }
     }
 
     private void createRepresentation() {
-
+        palette = new ArrayList<>();
+        indexesOfColors = new int[width][height];
     }
 
 }
