d51f1ef6accdbd9eb395530ed61f74d748a5cd42
l17004536 <dontstopmenow_12>
2018-11-30 11:31:07 +0100

    Finalisation de la classe VectorImage.

--- src/main/java/image/VectorImage.java
+++ src/main/java/image/VectorImage.java
@@ -1,4 +1,23 @@
 package image;
 
-public class VectorImage implements Image {
+import javafx.scene.paint.Color;
+
+import java.util.List;
+
+public class VectorImage extends RasterImage{
+
+    private List<Shape> shapes;
+
+    public VectorImage(List<Shape> shapes, int width, int height){
+        this.shapes = shapes;
+        this.width = width;
+        this.height = height;
+    }
+
+    public Color getPixelColor(int x, int y){
+        for(int i=0;i<shapes.size();i++){
+            if (shapes.get(i).contains(new Point(x,y)))return shapes.get(i).getColor();
+        }
+        return Color.WHITE;
+    }
 }
