82edafe97dab6890ec9e46d12942234270cf68f3
l17004536 <dontstopmenow_12>
2018-11-30 11:47:41 +0100

    Création de la class circle.

---
+++ src/main/java/image/Circle.java
@@ -0,0 +1,12 @@
+package image;
+
+
+import javafx.scene.paint.Color;
+
+public class Circle implements Shape {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    Color color;
+}
