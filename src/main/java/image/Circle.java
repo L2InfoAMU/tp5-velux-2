48bdc9785ac1b3dfc8693b242b6cd43c1e584af4
l17004536 <dontstopmenow_12>
2018-12-05 10:39:12 +0100

    Création de Circle

--- src/main/java/image/Circle.java
+++ src/main/java/image/Circle.java
@@ -6,7 +6,23 @@ import javafx.scene.paint.Color;
 public class Circle implements Shape {
     private int x;
     private int y;
-    private int width;
-    private int height;
+    private int rayon;
     Color color;
+
+    public Circle(int x, int y, int rayon, Color color){
+        this.x = x;
+        this.y = y;
+        this.rayon = rayon;
+        this.color = color;
+    }
+
+    @Override
+    public boolean contains(Point point) {
+        if (point.x<x-rayon || point.x>x+rayon && point.y<y-rayon || point.y>y+rayon) return false;
+        return true;
+    }
+    public Color getColor() {
+        return color;
+    }
+
 }
---
+++ src/main/java/image/TriangleRectangle.java
@@ -0,0 +1,25 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+public class TriangleRectangle implements Shape {
+    private int x;
+    private int y;
+    private int side1;
+    private int side2;
+    Color color;
+
+    public TriangleRectangle(int x, int y, int side1, int side2, Color color){
+        this.x = x;
+        this.y = y;
+        this.side1 = side1;
+        this.side2 = side2;
+        this.color = color;
+    }
+
+    public boolean contains(Point point) {
+        if(
+
+                )
+    }
+}
